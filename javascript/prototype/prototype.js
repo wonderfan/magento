1f484df92cfa7c02bf180f91a686f558d7f095f3
Administrator <Administrator@8BNGWRX510TQX3G>
2013-07-23 21:31:47 +0800

    ZF project

---
+++ javascript/prototype/prototype.js
@@ -0,0 +1,11 @@
+var myClass = Class.create({
+    initialize:function(){
+        this.values = {};
+        this.name ="china";
+        this.get = function(key){
+            return this.values[key];
+        }
+    },
+    echo: function(message){ console.log(message);}    
+    
+});
