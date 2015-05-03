740f41bb048fe35126e27a262e76c89b7c1a1c33
阿樊提 <fanjiahe2000@163.com>
2015-05-03 09:38:19 +0800

    Update scroll-to-top.js

--- javascript/jquery/scroll-to-top.js
+++ javascript/jquery/scroll-to-top.js
@@ -1,3 +1,10 @@
+/*
+|--------------------------------------------------------------------------
+| UItoTop jQuery Plugin 1.1
+| http://www.mattvarone.com/web-design/uitotop-jquery-plugin/
+|--------------------------------------------------------------------------
+*/
+
 (function($){
 	$.fn.UItoTop = function(options) {
  		var defaults = {
