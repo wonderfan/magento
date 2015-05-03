08923861213d1f34d88f28e9fce2dcae87dc6909
阿樊提 <fanjiahe2000@163.com>
2015-05-03 09:37:20 +0800

    Create scroll-to-top.js

---
+++ javascript/jquery/scroll-to-top.js
@@ -0,0 +1,36 @@
+(function($){
+	$.fn.UItoTop = function(options) {
+ 		var defaults = {
+			min: 200,
+			inDelay:600,
+			outDelay:400,
+  			containerID: 'toTop',
+			containerHoverID: 'toTopHover',
+			scrollSpeed: 1200,
+			easingType: 'linear'
+ 		};
+ 		var settings = $.extend(defaults, options);
+		var containerIDhash = '#' + settings.containerID;
+		var containerHoverIDHash = '#'+settings.containerHoverID;
+		$('body').append('<a href="#" id="'+settings.containerID+'"></a>');
+		$(containerIDhash).hide().click(function(){
+			$('html, body').animate({scrollTop:0}, settings.scrollSpeed, settings.easingType);
+			$('#'+settings.containerHoverID, this).stop().animate({'opacity': 0 }, settings.inDelay, settings.easingType);
+			return false;
+		})
+		.prepend('<span id="'+settings.containerHoverID+'"><i class="fa fa-angle-up"></i></span>')
+		$(window).scroll(function() {
+			var sd = $(window).scrollTop();
+			if(typeof document.body.style.maxHeight === "undefined") {
+				$(containerIDhash).css({
+					'position': 'absolute',
+					'top': $(window).scrollTop() + $(window).height() - 50
+				});
+			}
+			if ( sd > settings.min ) 
+				$(containerIDhash).fadeIn(settings.inDelay);
+			else 
+				$(containerIDhash).fadeOut(settings.Outdelay);
+		});
+	};
+})(jQuery);
