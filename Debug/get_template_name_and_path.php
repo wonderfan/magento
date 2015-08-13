18784bf665dba5e086bb1000184d364e56c0f767
阿樊提 <fanjiahe2000@163.com>
2015-08-13 22:44:27 +0800

    Update get_template_name_and_path.php

--- Debug/get_template_name_and_path.php
+++ Debug/get_template_name_and_path.php
@@ -2,3 +2,7 @@
 echo $this->getChild($name)->getTemplate();
 
 echo $this->getLayout()->getBlock($name)->getTemplate();
+
+echo $this->getChild($name)->getTemplateFile();
+
+echo $this->getLayout()->getBlock($name)->getTemplateFile();
